3d4d6d4bc576ab387ead64d4b7c75f4b17ba7f1c
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-25 15:43:51 -0300

    feat: adicionar componente exemplo com styled-components

---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	display: flex;
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const Title = styled.Text`
+	color: black;
+	font-size: 24px;
+`;
