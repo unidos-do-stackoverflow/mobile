a696fac9e64c5c038cb7a59ee2abc88187f8364d
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-25 23:53:23 -0300

    feat: implementar navegação de Register para Login

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { Container, Title, Btn, BtnText } from './styles';
+
+type RegisterScreenRouteProp = RouteProp<RootStackParamList, 'Register'>;
+type RegisterScreenNavigationProp = StackNavigationProp<
+	RootStackParamList,
+	'Register'
+>;
+
+type Props = {
+	route: RegisterScreenRouteProp;
+	navigation: RegisterScreenNavigationProp;
+};
+
+export default function Register({ navigation, route }: Props) {
+	function navigateToLogin() {
+		navigation.navigate('Login');
+	}
+
+	return (
+		<Container>
+			<Title>[Tela de Cadastro]</Title>
+			<Btn onPress={navigateToLogin}>
+				<BtnText>Já tenho cadastro</BtnText>
+			</Btn>
+		</Container>
+	);
+}
---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	display: flex;
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const Title = styled.Text`
+	color: black;
+	font-size: 24px;
+`;
+
+export const Btn = styled.TouchableOpacity`
+	width: 200px;
+	height: 50px;
+	justify-content: center;
+	align-items: center;
+	background-color: black;
+`;
+
+export const BtnText = styled.Text`
+	color: white;
+	font-size: 18px;
+`;
