a55ae7c2d54336c5c3f871eaa1a23f826cf1bfb9
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-25 23:52:36 -0300

    feat: adicionar react-navigation às rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function Routes() {
 	return (
-		<>
-			<Login />
-		</>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name='Login'
+					component={Login}
+					options={{ headerShown: false }}
+				/>
+
+				<Stack.Screen
+					name='Register'
+					component={Register}
+					options={{ headerShown: false }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
