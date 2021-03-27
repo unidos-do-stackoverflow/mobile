e5f546e4e7bb3aad5ac839ca10d6556e438455cc
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-27 14:32:43 -0300

    feat: adicionar implementação básica da barra de navegação inferior

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,28 +2,23 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RootStackParamList from '../utils/RootStackParamList';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 
 const Stack = createStackNavigator<RootStackParamList>();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function Routes() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen
-					name='Login'
-					component={Login}
-					options={{ headerShown: false }}
-				/>
-
-				<Stack.Screen
-					name='Register'
-					component={Register}
-					options={{ headerShown: false }}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
+		<>
+			<NavigationContainer>
+				<Tab.Navigator>
+					<Tab.Screen name='Login' component={Login} />
+					<Tab.Screen name='Register' component={Register} />
+				</Tab.Navigator>
+			</NavigationContainer>
+		</>
 	);
 }
