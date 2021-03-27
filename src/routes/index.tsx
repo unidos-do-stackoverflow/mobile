66a5880277b3b57ddd3cad45e9137592986df073
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-27 13:58:09 -0300

    fix: importar RootStackParamList no arquivo de rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import RootStackParamList from '../utils/RootStackParamList';
 
 import Login from '../pages/Login';
 import Register from '../pages/Register';
