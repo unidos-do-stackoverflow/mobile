e40c44c6673cf83765dc9e9745791f32226ea950
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-27 15:31:32 -0300

    feat: adicionar ícones à navbar

--- src/routes/main.routes.tsx
+++ src/routes/main.routes.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import RootStackParamList from '../utils/RootStackParamList';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {
+	MaterialCommunityIcons,
+	FontAwesome5,
+	Feather,
+} from '@expo/vector-icons';
 
 import TestScreen from '../pages/Main/TestScreen';
 import Screen2 from '../pages/Main/Screen2';
@@ -13,9 +18,33 @@ const Tab = createMaterialBottomTabNavigator();
 export default function Routes() {
 	return (
 		<Tab.Navigator>
-			<Tab.Screen name='Tela 1' component={Screen1} />
-			<Tab.Screen name='Tela 2' component={Screen2} />
-			<Tab.Screen name='Tela 3' component={TestScreen} />
+			<Tab.Screen
+				name='Feed'
+				component={Screen1}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<MaterialCommunityIcons name='post' size={24} color='white' />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Doação'
+				component={Screen2}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<FontAwesome5 name='donate' size={24} color='white' />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Configurações'
+				component={TestScreen}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<Feather name='settings' size={24} color='white' />
+					),
+				}}
+			/>
 		</Tab.Navigator>
 	);
 }
