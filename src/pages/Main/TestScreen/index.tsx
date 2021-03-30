6722f2ba6a73f09c7d5fa57ce2e582922490dfca
ZFRenato <renatopscarlos@gmail.com>
2021-03-30 01:04:27 -0300

    feat: Adicionar Avatar, descrição, botão edição perfil.

--- src/pages/Main/TestScreen/index.tsx
+++ src/pages/Main/TestScreen/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Container } from './styles';
+import { Container, 
+	ContainerHeader, 
+	Title, 
+	TextDesc,
+	BottonEditarPerfil,
+ } from './styles';
 import { MaterialCommunityIcons, FontAwesome5, Feather} from '@expo/vector-icons';
 import RootStackParamList from '../../../utils/RootStackParamList';
-import { Button, Appbar } from 'react-native-paper';
+import { Button, Appbar, Avatar, Text } from 'react-native-paper';
 import { useAuth } from '../../../context/auth';
+import { State } from 'react-native-gesture-handler';
 
 type LoginScreenRouteProp = RouteProp<RootStackParamList, 'Login'>;
 type LoginScreenNavigationProp = StackNavigationProp<
@@ -19,18 +25,31 @@ type Props = {
 	navigation: LoginScreenNavigationProp;
 };
 
+	
+
 export default function Login({ route, navigation }: Props) {
-	const { signOutUser } = useAuth();
-	const goBack = ()=> console.log('back action')
-	const config = ()=> console.log('settings')
+	//const { signOutUser } = useAuth();
+	const user = {
+		name: 'Anna Clara',
+		photo: require('../../../../assets/Anna_Clara.jpg'),
+		description: 'Mãe de 3 filhos, 32 anos, procurando ajuda e adoiando quando posso'
+	};
+	const goBack = ()=> console.log('back action');
+	const config = ()=> console.log('settings');
+	
 
 	return (
 		<Container>
 			<Appbar.Header style={{justifyContent:'space-between', backgroundColor: '#fff'}}>
 				<Appbar.BackAction style={{marginLeft: 10}} onPress={goBack}/>
 				<Appbar.Action icon="cog" style={{marginRight: 10}} onPress={config}/>
-
 			</Appbar.Header>
+			<ContainerHeader>
+				<Avatar.Image size={100} source={user.photo} style={{marginTop: 15}}/>
+				<Title>{user.name}</Title>
+				<TextDesc>{user.description}</TextDesc>
+				<BottonEditarPerfil><Text style={{ color: '#808080', fontSize: 15, fontStyle: 'italic'}}>Editar perfil</Text></BottonEditarPerfil>
+			</ContainerHeader>
 			
 		</Container>
 	);
--- src/pages/Main/TestScreen/styles.ts
+++ src/pages/Main/TestScreen/styles.ts
@@ -3,4 +3,30 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
 	flex: 1;
 	margin: 0px 0px;
+	background-color: #fff;
 `;
+
+export const ContainerHeader = styled.View`
+	flex : 1;
+	align-items: center;
+	text-align: center;
+`;
+
+export const Title = styled.Text`
+	font-weight: bold;
+	font-size: 30px;
+	letter-spacing: 1.5px;
+	margin-top: 10px;
+`;
+
+export const TextDesc = styled.Text`
+	font-size: 15px;
+	margin: 3px 25px 5px 25px ;
+	text-align: center;
+	font-weight: 400;
+`;
+
+export const BottonEditarPerfil = styled.TouchableOpacity`
+`; 
+
+
