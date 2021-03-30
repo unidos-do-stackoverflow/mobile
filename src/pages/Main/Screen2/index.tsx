e045e0c413446ccf53a7059beedf915b96718f22
Matheus Montenegro <matheusalbuquerquemontenegro@hotmail.com>
2021-03-29 21:36:03 -0300

    feat: implementar o like

--- src/pages/Main/Screen2/index.tsx
+++ src/pages/Main/Screen2/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
-import { Container } from './styles';
+import { StyleSheet } from 'react-native'
+import { Text } from 'react-native-paper'
+import { Container, Btn } from './styles';
 
-export default function Screen2() {
-	return <Container></Container>;
+
+export default function Post() {
+	const [like, setLike] = React.useState(0);
+	const increment = () => {
+		setLike(like+1)
+	}
+	return <Container>
+		<Text style={styles.text}>{like}</Text>
+		<Btn onPress={increment}>
+			<Text>Apoiar</Text>
+		</Btn>
+	</Container>;
 }
+
+const styles = StyleSheet.create({
+	text:{
+		fontSize:26,
+	}
+});
--- src/pages/Main/Screen2/styles.ts
+++ src/pages/Main/Screen2/styles.ts
@@ -6,3 +6,13 @@ export const Container = styled.View`
 	align-items: center;
 	background-color: #4ca77b;
 `;
+
+export const InputFields = styled.View`
+	margin: 10px 0;
+	height: 300px;
+`;
+
+export const Btn = styled.TouchableOpacity`
+	color:white;
+	font-size:20px;
+`;
