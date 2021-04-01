3920ffdb76dccd19ce1d5e58774743dd2f81d8ee
Isabelar07 <isabelarochas@outlook.com.br>
2021-04-01 12:09:00 -0300

    feat: useEffect com mock da requisição de filhos do usuário

--- src/pages/Main/Screen1/index.tsx
+++ src/pages/Main/Screen1/index.tsx
@@ -1,6 +1,35 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState, useEffect } from 'react';
+import { Container, Text } from './styles';
+
 
 export default function Screen1() {
-	return <Container></Container>;
+
+	const [children, setChildren] = useState([{} as any]);
+
+	useEffect(() => {
+		// requisição de API
+		setChildren([
+			{
+				name: 'Maria',
+				photo: 'http//...'
+			},
+			{
+				name: 'João',
+				photo: 'http//...'
+			}
+		]) 
+	}, [])
+
+
+	return (
+	
+	<Container>
+
+		<Text>Pedido de doação</Text>
+		<Text>{children[0].name}</Text>
+		
+
+	</Container>
+	
+	);
 }
