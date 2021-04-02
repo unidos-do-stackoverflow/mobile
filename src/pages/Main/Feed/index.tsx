9e061dd289cbff851f91988efdeb494b6d027e97
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-02 12:27:34 -0300

    feat: adicionar FlatList à página Feed

--- src/pages/Main/Feed/index.tsx
+++ src/pages/Main/Feed/index.tsx
@@ -8,12 +8,31 @@ import {
 	IconTextWrapper,
 } from './styles';
 import Post from '../../../components/Post';
+import { FlatList, ListRenderItem } from 'react-native';
 
 // Icons
 import { AntDesign } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 
+import IPostProps from '../../../components/Post/interfaces/IPostProps';
+
 export default function Feed() {
+	const DATA = [
+		{
+			id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+			username: 'Juliana Mazini',
+			date: 20,
+			content: `Meu pequeninho estudando...
+
+Bom dia gente, me chamo Juliana Mazini, e estou precisando de ajuda para custear os estudos do meu filho. Por favor, se alguém puder colaborar, estou aceitando doações para poder custeá-lo, obrigada :)`,
+		},
+	];
+
+	const renderPost: ListRenderItem<IPostProps> = ({ item }) => {
+		const { username, content, date } = item;
+		return <Post username={username} content={content} date={date} />;
+	};
+
 	return (
 		<Container>
 			<Header>
@@ -31,12 +50,11 @@ export default function Feed() {
 					<Location>Santos, SP</Location>
 				</IconTextWrapper>
 			</LocationContainer>
-			<Post
-				username={'Juliana Mazini'}
-				date={20}
-				content={`Meu pequeninho estudando...
 
-Bom dia gente, me chamo Juliana Mazini, e estou precisando de ajuda para custear os estudos do meu filho. Por favor, se alguém puder colaborar, estou aceitando doações para poder custeá-lo, obrigada :)`}
+			<FlatList
+				data={DATA}
+				renderItem={renderPost}
+				keyExtractor={(item) => item.id}
 			/>
 		</Container>
 	);
