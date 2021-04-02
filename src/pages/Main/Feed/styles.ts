696bca3065b56989ab33599f8456d9b055d705ad
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-02 09:47:57 -0300

    feat: adicionar componentes para cabeçalho e título do cabeçalho do Feed

--- src/pages/Main/Feed/styles.ts
+++ src/pages/Main/Feed/styles.ts
@@ -2,6 +2,20 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
 	flex: 1;
-	justify-content: center;
+	justify-content: flex-start;
 	align-items: center;
+`;
+
+export const Header = styled.View`
+	background-color: #3bb273;
+	width: 100%;
+	height: 50px;
+	justify-content: center;
+`;
+
+export const HeaderTitle = styled.Text`
+	font-size: 18px;
+	font-weight: bold;
+	color: whitesmoke;
+	margin-left: 20px;
 `;
