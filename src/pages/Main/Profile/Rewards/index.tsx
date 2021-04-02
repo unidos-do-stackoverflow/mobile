be3bbbaa983f83b55a92ff81f47cdbe7b008dd78
ZFRenato <renatopscarlos@gmail.com>
2021-04-02 17:58:32 -0300

    feat: Adicionando gift Card.

--- src/pages/Main/Profile/Rewards/index.tsx
+++ src/pages/Main/Profile/Rewards/index.tsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text, View, StyleSheet } from 'react-native';
 import { Avatar, ProgressBar, Colors } from 'react-native-paper';
 import { 
 	Container,
 	Header,
+	ContainerRewards,
+	LogoService,
  } from './styles';
- import { FontAwesome5, Ionicons } from '@expo/vector-icons';
-import styled from 'styled-components';
+ import { FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function index() {
+		// Gift Card of conponet's
+
+		const ComponentGiftCard = ()=>{
+			return (
+
+				<View style={styles.BoxGiftStyle}>
+					<View style={{flexDirection: 'row', marginBottom: 15}}>
+						<LogoService>
+							<MaterialCommunityIcons name="netflix" size={40} color="#fff" />
+						</LogoService>
+						<View style={{width: 180, marginLeft: 10}}>
+							<Text style={{fontSize: 18, fontWeight: '700'}}>
+								2 meses Netflix
+							</Text>
+							<Text style={{fontSize: 11, fontWeight: '400'}}>
+								Dá para ver todas as temporadas de
+								sua série inteira em 2 meses!
+							</Text>
+						</View>											
+					</View>
+					<View style={{flexDirection: 'row', position: 'absolute', bottom: 15, right: 15}}>
+						<FontAwesome5
+								name="coins"
+								size={24}
+								color="#F1C40F"
+						/>
+						<Text style={{fontWeight:"700", marginLeft: 4}}>x1000 pts</Text>
+					</View>
+				</View>
+			);
+		}
+	
+
 	return (
 		<Container>
 			<Header>
@@ -76,6 +112,20 @@ export default function index() {
 					</View>
 				</View>
 			</Header>
+			<ContainerRewards>
+				<Text
+					style={{fontSize: 18, fontWeight: '800'}}
+				>
+					Gift Cards 					
+				</Text>
+				<SafeAreaView>
+					<ScrollView>
+							<ComponentGiftCard/>
+					</ScrollView>
+				</SafeAreaView>
+
+			</ContainerRewards>
+			
 		</Container>
 	);
 }
@@ -86,6 +136,28 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between',
 		alignItems: 'center',
 	},
+	BoxGiftStyle:{
+		paddingHorizontal: 15,
+		paddingVertical: 15,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 9,
+		},
+		backgroundColor: '#F0F3F4',
+		padding: 10,
+		borderRadius: 14,
+		borderBottomWidth: 4,
+		borderBottomColor: '#979A9A',
+		borderStyle: 'solid',
+	},
+
+	pad:{
+		padding: 20,
+	},
+
+
+
 });
 
 
--- src/pages/Main/Profile/Rewards/styles.ts
+++ src/pages/Main/Profile/Rewards/styles.ts
@@ -8,3 +8,20 @@ export const Header = styled.View`
 	background-color: #3BB273;
 	padding: 20px;
 `;
+export const ContainerRewards = styled.View`
+	flex: 1;
+	padding: 20px;
+	background-color: #fff;
+`;
+
+export const LogoService = styled.View`
+	width: 80px;
+	height: 80px;
+	background-color: #008282;
+	align-items: center;
+	justify-content: center;
+`;
+
+
+
+
