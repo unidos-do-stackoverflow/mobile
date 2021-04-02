73d29b1d1f64baadece3f62a64a4e9f090815890
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-02 09:53:35 -0300

    feat: adicionar ícone de pesquisa

--- src/pages/Main/Feed/index.tsx
+++ src/pages/Main/Feed/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Container } from './styles';
+import { Container, Header, HeaderTitle } from './styles';
+import Post from '../../../components/Post';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Feed() {
 	return (
 		<Container>
-			<Text>News Feed</Text>
+			<Header>
+				<HeaderTitle>Lápis e Borracha</HeaderTitle>
+				<AntDesign
+					name='search1'
+					size={20}
+					color='white'
+					style={{ marginRight: 20 }}
+				/>
+			</Header>
+			<Post
+				username={'Juliana Mazini'}
+				content={`Meu pequeninho estudando ...
+				Bom dia gente, me chamo Anna clara, e estou  precisando de ajuda para custear 
+				os estudos do meu filho. Por favor, se alguém puder colaborar, estou aceintando 
+				doações para poder custea-lo, obrigado :)`}
+			/>
 		</Container>
 	);
 }
