25f33bf86067d2ccb86a9709882fa4800d371559
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-02 09:47:18 -0300

    feat: colorizar StatusBar com cor primária do app

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/auth';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
 
 declare global {
 	namespace ReactNativePaper {
@@ -29,6 +30,8 @@ const theme = {
 	},
 };
 
+setStatusBarBackgroundColor('#3bb273', true);
+
 export default function App() {
 	return (
 		<SafeAreaProvider>
@@ -45,6 +48,7 @@ export default function App() {
 					</NavigationContainer>
 				</AuthProvider>
 			</SafeAreaView>
+			<StatusBar style='light' />
 		</SafeAreaProvider>
 	);
 }
