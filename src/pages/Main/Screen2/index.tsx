ce4c5e61a8422efe9de5699b545c6d292f728409
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-02 06:58:25 -0300

    fix: embrulhar texto dentro de TouchableOpacity com tag <Text>

--- src/pages/Main/Screen2/index.tsx
+++ src/pages/Main/Screen2/index.tsx
@@ -1,57 +1,56 @@
 import React from 'react';
-import { StyleSheet, Image} from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 import { Text, TextInput } from 'react-native-paper';
 import { Container, Btn, BtnEnviar } from './styles';
 
-interface Props{
-	nameUser:string;
-	count:number;
-	file:string;
-	content:string;
+interface Props {
+	nameUser: string;
+	count: number;
+	file: string;
+	content: string;
 }
 
-
-export default function Post({nameUser, count, content}:Props) {
+export default function Post({ nameUser, count, content }: Props) {
 	//Like
 	const [like, setLike] = React.useState(0);
 	const increment = () => {
-		setLike(like+1)
-	}
+		setLike(like + 1);
+	};
 	//Comentários
-	const allComents = []
+	const allComents = [];
 	const [comments, setComments] = React.useState('');
-    const sumComments = () =>{
-        allComents.push(' ')
-    };
+	const sumComments = () => {
+		allComents.push(' ');
+	};
 
-	return <Container>
+	return (
+		<Container>
+			<Text>{nameUser}</Text>
 
-		<Text>{nameUser}</Text>
+			<Text style={styles.text}> Há {count} min</Text>
 
-		<Text style={styles.text}> Há {count} min</Text>
+			<Text>{content}</Text>
 
-		<Text>{content}</Text>
+			<Text style={styles.text}>{like}</Text>
+			<Btn onPress={increment}>
+				<Text>Apoiar</Text>
+			</Btn>
 
-		<Text style={styles.text}>{like}</Text>
-		<Btn onPress={increment}>
-			<Text>Apoiar</Text>
-		</Btn>
-
-		<TextInput 
-            value={comments}
-            onChangeText={(text) => setComments(text)}
-        />
-        <BtnEnviar onPress={sumComments}> Enviar </BtnEnviar>
-        <Text>{allComents.length} Comentários</Text>
-	</Container>;
+			<TextInput value={comments} onChangeText={(text) => setComments(text)} />
+			<BtnEnviar onPress={sumComments}>
+				<Text>Enviar</Text>
+			</BtnEnviar>
+			<Text>{allComents.length} Comentários</Text>
+		</Container>
+	);
 }
 
 const styles = StyleSheet.create({
-	text:{
-		fontSize:18,
+	text: {
+		fontSize: 18,
 	},
-	image:{
-		height:180,
-		width:180,
-	}
+	image: {
+		height: 180,
+		width: 180,
+	},
 });
