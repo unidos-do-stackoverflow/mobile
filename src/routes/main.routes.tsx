b56e58972a6891ff7b8304e9216877da42f19b49
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-03 15:51:27 -0300

    feat: adicionar botão na tab bar inferior envolvido por TouchableOpacity

--- src/routes/main.routes.tsx
+++ src/routes/main.routes.tsx
@@ -6,6 +6,8 @@ import TestScreen from '../pages/Main/TestScreen';
 import Screen1 from '../pages/Main/Screen1';
 import Screen2 from '../pages/Main/Screen2';
 import CreateChildren from '../pages/Main/CreateChildren';
+import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Tab = createBottomTabNavigator();
 
@@ -27,8 +29,29 @@ export default function Routes() {
 				name='Criar post'
 				component={CreateChildren}
 				options={{
-					tabBarIcon: ({ color }) => (
-						<Ionicons name='add-circle' size={30} color='#4d4d4d' />
+					tabBarButton: (props) => (
+						<TouchableOpacity
+							{...props}
+							style={{
+								bottom: 30, // space from bottombar
+								height: 70,
+								width: 70,
+								borderRadius: 58,
+								justifyContent: 'center',
+								alignItems: 'center',
+							}}
+						>
+							<Ionicons
+								name='add-circle'
+								size={60}
+								color='#4d4d4d'
+								style={{
+									width: 60,
+									height: 60,
+									alignContent: 'center',
+								}}
+							/>
+						</TouchableOpacity>
 					),
 				}}
 			/>
