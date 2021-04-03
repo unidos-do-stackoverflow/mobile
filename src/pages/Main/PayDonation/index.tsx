f8b8d4b10c37be93dfbf2f6bf7772e506e06a8d9
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-03 19:05:53 -0300

    refactor: sinalizar início do fluxo de doação

--- src/pages/Main/PayDonation/index.tsx
+++ src/pages/Main/PayDonation/index.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Container } from './styles';
-import { Appbar } from 'react-native-paper';
 
 export default function PayDonation() {
-
-	function Main (){
-
-		return(
-			<>
-				<Appbar.Header>
-					
-				</Appbar.Header>
-			</>
-		);
-
-	}
-
 	return (
 		<Container>
-			<Text>Donation</Text>
+			<Text>Início do fluxo de doação</Text>
 		</Container>
 	);
 }
