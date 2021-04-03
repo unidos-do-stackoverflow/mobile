f4c095018818c27d8b159d8c1d85a368f5947147
Isabelar07 <isabelarochas@outlook.com.br>
2021-04-03 10:58:50 -0300

    style: estilizar página de cadastrar criança

---
+++ src/pages/Main/CreateChildren/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    justify-content: center;
+    height: 18px;
+    left: 20px;
+    right: 20px;
+    top: 300px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: bold;
+    font-size: 15px;
+    line-height: 18px;
+    color: #005556;
+    flex-grow: 0;
+    margin: 0px 10px;
+`
+
+export const ContainerIcon = styled.View`
+    align-items: center;
+`
+
+export const ContainerInput = styled.View`
+    display: flex;
+    flex-direction: row;
+`
+
+export const ContainerDateOfBirth = styled.View`
+    width: 50%;
+`
+
+export const ContainerGender = styled.View`
+    
+`
