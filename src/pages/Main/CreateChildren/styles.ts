8dc54f5073787dc4d61c2c6dbead52b50d8063d6
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-03 15:00:32 -0300

    fix: corrigir responsividade dos inputs de texto

--- src/pages/Main/CreateChildren/styles.ts
+++ src/pages/Main/CreateChildren/styles.ts
@@ -1,34 +1,22 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-    justify-content: center;
-    height: 18px;
-    left: 20px;
-    right: 20px;
-    top: 300px;
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: bold;
-    font-size: 15px;
-    line-height: 18px;
-    color: #005556;
-    flex-grow: 0;
-    margin: 0px 10px;
-`
+	flex: 1;
+	justify-content: center;
+	margin: 0px 15px;
+`;
 
 export const ContainerIcon = styled.View`
-    align-items: center;
-`
+	align-items: center;
+`;
 
 export const ContainerInput = styled.View`
-    display: flex;
-    flex-direction: row;
-`
+	display: flex;
+	flex-direction: row;
+`;
 
 export const ContainerDateOfBirth = styled.View`
-    width: 50%;
-`
+	width: 50%;
+`;
 
-export const ContainerGender = styled.View`
-    
-`
+export const ContainerGender = styled.View``;
