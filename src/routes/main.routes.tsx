659db6ddcd7d7de0a6b23cb43575a1230a492e81
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-03 16:02:03 -0300

    feat: adicionar boilerplate de chamada de modal  dentro de main.routes

--- src/routes/main.routes.tsx
+++ src/routes/main.routes.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons, Ionicons, Feather } from '@expo/vector-icons';
+import Modal from 'react-native-modal';
 
 import TestScreen from '../pages/Main/TestScreen';
 import Screen1 from '../pages/Main/Screen1';
@@ -12,48 +13,61 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 const Tab = createBottomTabNavigator();
 
 export default function Routes() {
+	const [isModalVisible, setModalVisibility] = useState(false);
+
 	return (
-		<Tab.Navigator
-			tabBarOptions={{ keyboardHidesTabBar: true, showLabel: false }}
-		>
-			<Tab.Screen
-				name='Feed'
-				component={Screen2}
-				options={{
-					tabBarIcon: ({ color }) => (
-						<MaterialCommunityIcons name='post' size={24} color='#4d4d4d' />
-					),
-				}}
-			/>
-			<Tab.Screen
-				name='Criar post'
-				component={CreateChildren}
-				options={{
-					tabBarButton: (props) => (
-						<TouchableOpacity {...props} style={{ alignItems: 'center' }}>
-							<Ionicons
-								name='add-circle'
-								size={45}
-								color='#008147'
-								style={{
-									width: 45,
-									height: 45,
-									alignContent: 'center',
-								}}
-							/>
-						</TouchableOpacity>
-					),
-				}}
-			/>
-			<Tab.Screen
-				name='Configurações'
-				component={TestScreen}
-				options={{
-					tabBarIcon: ({ color }) => (
-						<Feather name='settings' size={24} color='#4d4d4d' />
-					),
-				}}
-			/>
-		</Tab.Navigator>
+		<>
+			<Tab.Navigator
+				tabBarOptions={{ keyboardHidesTabBar: true, showLabel: false }}
+			>
+				<Tab.Screen
+					name='Feed'
+					component={Screen2}
+					options={{
+						tabBarIcon: ({ color }) => (
+							<MaterialCommunityIcons name='post' size={24} color='#4d4d4d' />
+						),
+					}}
+				/>
+				<Tab.Screen
+					name='Criar post'
+					component={CreateChildren}
+					options={{
+						tabBarButton: (props) => (
+							<TouchableOpacity
+								{...props}
+								style={{ alignItems: 'center' }}
+								onPress={() => setModalVisibility(!isModalVisible)}
+							>
+								<Ionicons
+									name='add-circle'
+									size={45}
+									color='#008147'
+									style={{
+										width: 45,
+										height: 45,
+										alignContent: 'center',
+									}}
+								/>
+							</TouchableOpacity>
+						),
+					}}
+				/>
+				<Tab.Screen
+					name='Configurações'
+					component={TestScreen}
+					options={{
+						tabBarIcon: ({ color }) => (
+							<Feather name='settings' size={24} color='#4d4d4d' />
+						),
+					}}
+				/>
+			</Tab.Navigator>
+			<Modal isVisible={isModalVisible}>
+				<View style={{ flex: 1 }}>
+					<Text>I am the modal content!</Text>
+				</View>
+			</Modal>
+		</>
 	);
 }
