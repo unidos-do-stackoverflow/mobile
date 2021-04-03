a56be1c243bd397c9c1c32dffe8ba39e5d4a799c
Matheus Montenegro <matheusalbuquerquemontenegro@hotmail.com>
2021-04-03 19:11:25 -0300

    feat: implementar a lista de crianças

--- src/pages/Main/Profile/Children/index.tsx
+++ src/pages/Main/Profile/Children/index.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import { View, StyleSheet, Image } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Text } from 'react-native-paper';
+import { Content, Title, Description, Header, List, ImageContent, Avatar } from './styles';
 
-export default function index() {
+interface Props{
+	childName:string;
+}
+
+export default function index({childName}:Props) {
+	const childList = {
+		name:childName
+	};
+
 	return (
-		<Container>
-			<Text>Crianças</Text>
-		</Container>
+		<View style={{backgroundColor:'#fff', flex:1}}>
+			<Content>
+				<Header>
+				<MaterialIcons name="child-care" size={48} color="#005556" />
+				<Title>
+					<Text style={{color:"#005556", fontWeight:'bold'}}>CRIANÇAS</Text>
+				</Title>
+				</Header>
+				<Description>
+					<Text style={{color:"#005556", fontWeight:'400'}}>Gerencie o cadastro das crianças</Text>
+				</Description>
+				<View>
+					<List>
+						<Text>{childList.name='Maria de Souza'}</Text>
+						<MaterialIcons name="account-circle" size={24} color="#005556" />
+					</List>
+					<List>
+						<Text>{childList.name='Fernando de Souza'}</Text>
+						<MaterialIcons name="account-circle" size={24} color="#005556" />
+					</List>
+					<List>
+						<Text>{childList.name='Carlos de Souza'}</Text>
+						<MaterialIcons name="account-circle" size={24} color="#005556" />
+						
+					</List>
+
+					<ImageContent>
+						<Image source={require('../../../../../assets/children1.jpg')}/>
+					</ImageContent>
+					
+
+				</View>
+				
+			</Content>
+		</View>
+			
 	);
 }
+
+const styles = StyleSheet.create ({
+});
--- src/pages/Main/Profile/Children/styles.ts
+++ src/pages/Main/Profile/Children/styles.ts
@@ -5,3 +5,44 @@ export const Container = styled.View`
 	justify-content: center;
 	align-items: center;
 `;
+
+export const Content = styled.View`
+	padding-left:70px;
+	padding-right:70px;
+`;
+
+export const Title = styled.View`
+	font-size:30px;
+	align-items:flex-start;
+	font-weight: 900;
+	color:#005556;
+	margin-left:12px;
+`;
+
+export const Header = styled.View`
+	flex-direction:row;
+	alignItems:center;
+	margin-top:50px;
+`;
+
+
+export const Description = styled.View`
+	font-size:24px;
+	color:#005556;
+	font-weight: 700;
+	margin-bottom:50px;
+`;
+
+export const List = styled.View`
+	flex-direction:row;
+	justify-content:space-between;
+	margin-bottom:30px;
+`;
+
+export const ImageContent = styled.View`
+	margin-top:125px;
+`;
+
+export const Avatar = styled.View`
+	borderRadius:90px;
+`;
