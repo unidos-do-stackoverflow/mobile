21658fa72bb415ab23357094a3e66e1584b117e3
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-03 19:04:41 -0300

    feat: adicionar novas rotas ao RootStackParamList

--- src/utils/RootStackParamList.ts
+++ src/utils/RootStackParamList.ts
@@ -5,6 +5,8 @@ type RootStackParamList = {
 	Children: undefined;
 	Rewards: undefined;
 	Vouchers: undefined;
+	Feed: undefined;
+	PayDonation: undefined;
 	// Feed: { sort: 'latest' | 'top' } | undefined;
 };
 
