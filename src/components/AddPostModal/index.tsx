afb655ec8e725bc1bf2e0026346b9298d74b7ef8
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-03 16:33:31 -0300

    feat: trocar ícone do botão p/ receber material

--- src/components/AddPostModal/index.tsx
+++ src/components/AddPostModal/index.tsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
-import { Container, ContainerCards, ContainerReceiver, Text, ContainerDonor, Description } from './styles';
+import {
+	Container,
+	ContainerCards,
+	ContainerReceiver,
+	Text,
+	ContainerDonor,
+	Description,
+} from './styles';
 import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
 
 export default function ModalAddPost() {
-
 	return (
-
-        <Container>
-
-
+		<Container>
 			<Text>Escolha o que gostaria de fazer</Text>
 
 			<ContainerCards>
-
-				<ContainerReceiver >
-					//TODO: trocar ícone
-					<FontAwesome name="file" size={20} color="#005556" /> 
-					<Description >Receber um material</Description>
+				<ContainerReceiver>
+					{/* //TODO: trocar ícone */}
+					<FontAwesome5 name='clipboard-list' size={40} color='#005556' />
+					<Description>Receber um material</Description>
 				</ContainerReceiver>
 
 				<ContainerDonor>
-					<FontAwesome5 name="hand-holding-heart" size={40} color="#005556" />
+					<FontAwesome5 name='hand-holding-heart' size={40} color='#005556' />
 					<Description>Doar um material</Description>
 				</ContainerDonor>
-			
 			</ContainerCards>
-
 		</Container>
-    );
+	);
 }
