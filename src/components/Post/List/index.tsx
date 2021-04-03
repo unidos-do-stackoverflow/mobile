b1e0304c32fa13d9083b477c9be1d57b550d2d05
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-03 19:05:29 -0300

    feat: adicionar hook useNavigation dentro do modal da Lista

--- src/components/Post/List/index.tsx
+++ src/components/Post/List/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Container, DonateButton, DonateButtonText } from './style';
+import { useNavigation } from '@react-navigation/native';
 
 export default function index() {
+	const navigation = useNavigation();
+
+	function navigateToPayDonation() {
+		navigation.navigate('PayDonation');
+	}
+
 	return (
 		<Container>
 			<DonateButton>
-				<DonateButtonText>Fazer doação</DonateButtonText>
+				<DonateButtonText onPress={navigateToPayDonation}>
+					Fazer doação
+				</DonateButtonText>
 			</DonateButton>
 		</Container>
 	);
