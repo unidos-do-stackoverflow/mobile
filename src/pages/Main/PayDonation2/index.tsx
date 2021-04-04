eba00bf26595022e506d86115e39928039683706
ZFRenato <renatopscarlos@gmail.com>
2021-04-04 15:19:07 -0300

    feat: Adição do header na tela de dados do cartão.

--- src/pages/Main/PayDonation2/index.tsx
+++ src/pages/Main/PayDonation2/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Container, DonateButton, DonateButtonText } from './styles';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
 export default function PayDonation2() {
@@ -10,9 +11,21 @@ export default function PayDonation2() {
 		navigation.navigate('PayDonation3');
 	}
 
+	const HeaderTitle = ()=>{
+		return(
+			<View style={{padding: 30, marginTop: 20}}>
+				<View style={{flexDirection: 'row', alignItems: 'center'}}>
+					<MaterialCommunityIcons name="hand-heart" size={33} color="#005556" />
+					<Text style={{fontSize: 18, fontWeight: '700', color: '#005556', marginLeft: 10}}>FAZER UMA DOAÇÃO</Text>
+				</View>
+				<Text style={{fontSize: 14, fontWeight: '700', color: '#005556', marginTop: 3}}>Preencha os dados do Cartão</Text>
+			</View>
+		);
+	};
+
 	return (
 		<Container>
-			<Text>Segunda tela do fluxo de doação</Text>
+			<HeaderTitle/>
 			<DonateButton onPress={navigateToPayDonation3}>
 				<DonateButtonText>Próxima tela</DonateButtonText>
 			</DonateButton>
